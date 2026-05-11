621dbb72c3f18fe81f4a6b6843dce4e6a98a10a4
Rahul <rahulchunduruu@gmail.com>
2026-05-11 10:53:21 +0530

    Add TAVILY_API_KEY retrieval and validation

--- config.py
+++ config.py
@@ -10,7 +10,11 @@ class Config:
     #XAI_API_KEY = os.getenv('XAI_API_KEY')
     OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
     XAI_API_KEY = st.secrets["XAI_API_KEY"]
+    TAVILY_API_KEY=st.secrets["TAVILY_API_KEY"]
     if not OPENAI_API_KEY:
         raise ValueError("OPENAI_API_KEY not found in .env file. Please add your API key.")
     if not XAI_API_KEY:
         raise ValueError("XAI_API_KEY not found in .env file. Please add your API key.")
+    if not TAVILY_API_KEY:
+        raise ValueError("TAVILY_API_KEY not found in .env file. Please add your API key.")
+    
